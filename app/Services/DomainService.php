18d4d4321804d84a408b19f8e955ccaa61e5cfc0
marion909 <marion909@users.noreply.github.com>
2025-11-21 10:53:59 +0100

    fix: Add missing Log facade import in DomainService

--- app/Services/DomainService.php
+++ app/Services/DomainService.php
@@ -7,6 +7,7 @@ use App\Models\Subdomain;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class DomainService
