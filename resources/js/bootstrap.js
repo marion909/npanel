9f4c056a2e7a20bc95ad5a0a4234b585ef4d00f8
marion909 <marion909@users.noreply.github.com>
2025-11-22 20:27:15 +0100

    fix: Use correct axios XSRF token configuration for version 1.6.4

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,4 +3,7 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
-window.axios.defaults.withXSRFToken = true;
+
+// CSRF Token handling for axios
+window.axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
+window.axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
